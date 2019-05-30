20b8dbca718497d7c5d99bf378b3cdccfd5aee5a
Erin <erinedawson@gmail.com>
2019-05-29 23:30:37 -0600

    remove extra parens from user input function

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const getUserChoice = userInput => {
   userInput = userInput.toLowerCase(); //deal with case-sensitivity
 
   //if statement, with cheat code
-  if(userInput === 'rock') || (userInput === 'paper') || (userInput === 'scissors') || (userInput === 'bomb'){
+  if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb'){
     return userInput;
   }else{
     return 'Please pick from the available options.'
